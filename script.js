9689ce36d04271f164b43ecade760e38396f7513
Dhruvil <dhruvilshah732@gmail.com>
2025-05-09 23:09:03 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const suprisebtn = document.getElementById('suprisebtn');
+const card = document.getElementById('card');
+
+suprisebtn.addEventListener('click', () => {
+    
+    card.classList.toggle('hidden');
+    
+});
